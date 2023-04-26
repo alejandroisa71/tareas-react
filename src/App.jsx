bf5d5bd4609be0f7f3b88507e9d36cd454e5e736
Tucuman <you@example.com>
2023-04-26 20:08:22 -0300

    Se agrega componete Basic par ir probando Formik

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { Routes, Route } from 'react-router-dom';
 import Heading from './components/Heading';
 import TareaForm from './components/TareaForm';
 import TareaList from './components/TareaList';
+import Basic from './components/Basic';
 
 import { ContextProvider } from './context/TareaContext';
 
 const App = () => {
   return (
-    <main className="bg-zinc-500 h-full">
+    <main className="bg-zinc-500 h-screen">
       <div className="container mx-auto p-10">
         <ContextProvider>
           <Heading />
@@ -15,6 +16,7 @@ const App = () => {
             <Route path="/" element={<TareaList />} />
             <Route path="/add" element={<TareaForm />} />
             <Route path="/edit/:id" element={<TareaForm />} />
+            <Route path="/otro" element={<Basic />} />
           </Routes>
         </ContextProvider>
       </div>
